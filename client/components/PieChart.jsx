74e953abe9dd965953b4efd5f3077a8a4e33f2e0
Nimrod Bahar <nimrodbahar@gmail.com>
2017-06-05 18:47:54 +0200

    added json loader and loaded initial json data to PieChart component

--- client/components/PieChart.jsx
+++ client/components/PieChart.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import exampleData from '../data/example.json';
+
 import '../styles/pie_chart.css';
 
 class PieChart extends Component {
   render() {
+    const items = exampleData.map((item) =>
+    <li key={item.cdn}>
+      {item.platform} traffic percentage:  {item.trafficPercentage}
+    </li>
+    );
     return(
       <div>
         <h2>One Day I will be a PieChart component</h2>
+        <div>
+          {console.log(exampleData)}
+        </div>
+        <ul>
+          {items}
+        </ul>
       </div>
     );
   }
